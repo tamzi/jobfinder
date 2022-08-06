29e648180f208371f6c2ab04e130e48e9e9b08d8
tamzi <tamrefrank@gmail.com>
2022-08-06 17:59:30 +0300

    Added paged routes

--- lib/ui/main.dart
+++ lib/ui/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import 'designsystem/atoms/jf_colours.dart';
 import 'designsystem/atoms/jf_texts.dart';
@@ -7,8 +8,27 @@ import 'designsystem/atoms/jf_texts.dart';
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'jobFinder',
+      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      home: HomePage(),
+      getPages: [
+        GetPage(name: '/page-one', page: () => PageOne()),
+        GetPage(name: '/page-two', page: () => PageTwo()),
+        GetPage(name: '/page-three', page: () => PageThree()),
+        GetPage(name: '/page-four/:data', page: () => PageFour()) // Dynamic route
+      ],
+    );
+  }
+}
+
+
+
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,5 +42,77 @@ class MyApp extends StatelessWidget {
         body: SafeArea(
           child: Container(),
         ));
+  }
+}
+
+// Page One
+class PageOne extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page One'),
+      ),
+      body: Center(
+        child: Text(
+          Get.parameters['id'] ?? 'Page One',
+          style: TextStyle(fontSize: 40),
+        ),
+      ),
+    );
+  }
+}
+
+// Page Two
+class PageTwo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page Two'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => Get.off(HomePage()),
+          child: Text('Go Home'),
+        ),
+      ),
+    );
+  }
+}
+
+// Page Three
+class PageThree extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page Three'),
+      ),
+      body: Center(
+        child: Text(
+          Get.parameters['id'] ?? 'Page Three',
+          style: TextStyle(fontSize: 40),
+        ),
+      ),
+    );
+  }
+}
+
+// Page Four
+class PageFour extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page Four'),
+      ),
+      body: Center(
+        child: Text(
+          Get.parameters['data'] ?? 'Page Four',
+          style: TextStyle(fontSize: 40),
+        ),
+      ),
+    );
   }
 }
